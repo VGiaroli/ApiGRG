6ce01b326a5ddc187964d0228e94a701137d2648
VGiaroli <valegiaroli2016@gmail.com>
2026-09-22 22:27:25 -0300

    inicio, actualizar perfil para el perfil

---
+++ Models/Usuario/ActualizarPerfilModel.cs
@@ -0,0 +1,9 @@
+namespace ApiGRG.Models.Usuario
+{
+    public class ActualizarPerfilModel
+    {
+        public string EmailNuevo { get; set; }
+        public string PasswordNueva { get; set; }
+        public string PasswordActual { get; set; }
+    }
+}
